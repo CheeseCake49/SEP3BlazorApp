99df4fc988c9b99c42f282016c7417cb0a9229cd
Luux2 <293428@viauc.dk>
2023-10-31 23:37:08 +0100

    swag tilføjet

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp1;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 builder.Services.AddScoped<ICenterService, CenterHttpClient>();
 
 builder.Services.AddScoped(
