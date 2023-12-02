946c6492d6de4f56969423d23f5753ba3f0da150
Emil Brügge <equistgaard@yahoo.dk>
2023-12-02 14:02:16 +0100

    Login and register added

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp1;
+using BlazorApp1.Auth;
+using BlazorApp1.Services.Http;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +15,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddScoped<ICenterService, CenterHttpClient>();
 builder.Services.AddScoped<ICourtService, CourtHttpClient>();
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+AuthorizationPolicies.AddPolicies(builder.Services);
 
 builder.Services.AddScoped(
     sp => 
